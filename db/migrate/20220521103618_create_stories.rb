1a6338bab027206b96e140c4166ae6c203841289
Caius Durling <dev@caius.name>
2022-05-21 11:58:31 +0100

    Start a story table

---
+++ db/migrate/20220521103618_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stories do |t|
+      t.string :url
+      t.string :title
+      t.integer :submitter_id
+
+      t.timestamps
+    end
+  end
+end
